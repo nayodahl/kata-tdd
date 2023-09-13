ce36121cbdee0a5d3fb66beff3e4b9a9cbacf60e
Anthony FACHAUX <a.fachaux@archerydata.com>
2023-09-13 10:16:13 +0200

    fourth iteration - refactor

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -2,25 +2,30 @@
 
 class FizzBuzz
 {
-    const FizzMultiplier = 3;
-
-    const BuzzMultiplier = 5;
-
+    const FIZZ_MULTIPLIER = 3;
+    const BUZZ_MULTIPLIER = 5;
+    const FIZZ = 'Fizz';
+    const BUZZ = 'Buzz';
 
     public function print(int $number): string
     {
-        if (($number % self::FizzMultiplier === 0) && ($number % self::BuzzMultiplier === 0)) {
-            return 'FizzBuzz';
+        if ($this->isMultipleOf($number, self::FIZZ_MULTIPLIER) && $this->isMultipleOf($number, self::BUZZ_MULTIPLIER)) {
+            return self::FIZZ . self::BUZZ;
         }
 
-        if ($number % self::FizzMultiplier === 0) {
-            return 'Fizz';
+        if ($this->isMultipleOf($number, self::FIZZ_MULTIPLIER)) {
+            return self::FIZZ;
         }
 
-        if ($number % self::BuzzMultiplier === 0) {
-            return 'Buzz';
+        if ($this->isMultipleOf($number, self::BUZZ_MULTIPLIER)) {
+            return self::BUZZ;
         }
 
         return (string)$number;
+    }
+
+    private function isMultipleOf(int $number, int $divider): bool
+    {
+        return ($number % $divider === 0);
     }
 }
